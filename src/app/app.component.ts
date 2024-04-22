c7f0431f302f6cfb65e10103469e4705ce36169d
Celal Karakoc <c.karakoc@live.nl>
2024-04-22 18:26:46 +0200

    Tutorial Hell 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {TodoListItem} from "./todo/todo-list-item.component";
+import {HomeComponent} from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TodoListItem, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/todo/todo-list-item.component.ts
+++ src/app/todo/todo-list-item.component.ts
@@ -2,5 +2,20 @@ import {Component} from "@angular/core";
 
 @Component({
   standalone: true,
-
+  selector: 'todo-list-item',
+  template: ' <p>(TODO) {{ taskTitle }}</p>',
+  styles: ['li { color: darkred; }'],
 })
+
+export class TodoListItem {
+  taskTitle = 'Read cup of coffee introduction';
+  isComplete = false;
+
+  updateTitle(newTitle: string) {
+    this.taskTitle = newTitle;
+  }
+
+  completeTask() {
+    this.isComplete = true;
+  }
+}
