df14fde07007e07725d528607680be70e1b9af73
Celal Karakoc <c.karakoc@live.nl>
2024-04-23 13:34:55 +0200

    Tutorial Hell 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
+
 import { TodoListItem } from "./todo/todo-list-item.component";
 import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TodoListItem, HomeComponent],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet, TodoListItem, HomeComponent, RouterLink],
+  // templateUrl: './app.component.html',
+  template: `
+    <a [routerLink]="['/']">
+      <header>
+        <section>
+          <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+        </section>
+      </header>
+    </a>
+    <router-outlet/>
+    <!--    <todo-list-item/>-->
+
+    <!--    <app-home/>-->`,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from "@angular/common"; // need to be imported in standalone
+import { RouterLink } from "@angular/router";
+
 import { HousingLocationComponent } from "../housing-location/housing-location.component";
 import { HousingLocation } from "../interfaces/housinglocation";
+import { HousingService } from "../housing.service";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HousingLocationComponent],
+  imports: [HousingLocationComponent, CommonModule, RouterLink],
   template: `
-    <section>
-      <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
-    </section>
     <section>
       <form>
         <input type="text" placeholder="Filter by city">
@@ -17,23 +18,20 @@ import { HousingLocation } from "../interfaces/housinglocation";
       </form>
     </section>
     <section class="results">
-      <app-housing-location [housingLocation]="housingLocation"></app-housing-location>
+      <app-housing-location
+        *ngFor="let housingLocation of housingLocationList"
+        [housingLocation]="housingLocation">
+      </app-housing-location>
     </section>
   `,
   styleUrl: './home.component.css'
 })
 
 export class HomeComponent {
-  readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
+  housingLocationList: HousingLocation[] = [];
+  housingService: HousingService = inject(HousingService);
 
-  housingLocation: HousingLocation = {
-    id: 9999,
-    name: 'Test Home',
-    city: 'Test city',
-    state: 'ST',
-    photo: `${this.baseUrl}/example-house.jpg`,
-    availableUnits: 99,
-    wifi: true,
-    laundry: false,
-  };
+  constructor() {
+    this.housingLocationList = this.housingService.getAllHousingLocations();
+  }
 }
